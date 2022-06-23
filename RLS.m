a2c0e379739ed4a50a71f3388bfa4a428d9ae485
Ploita <72399695+Ploita@users.noreply.github.com>
2022-06-23 09:53:59 -0300

    Add files via upload

--- RLS.m
+++ RLS.m
@@ -59,18 +59,18 @@ for i = 1:N-ny
 end
 
 %%
-k = 10;
+rho = 10;
 %Gráficos de Validação
-lim_inf = 2+k;
+lim_inf = 2+rho;
 lim_sup = N;
 
 figure
 subplot(2,1,1)
-plot(lim_inf:lim_sup,out_val(lim_inf:lim_sup)-y_cha(k,lim_inf:lim_sup)')
+plot(lim_inf:lim_sup,out_val(lim_inf:lim_sup)-y_cha(rho,lim_inf:lim_sup)')
 title('Sinal diferença')
 
 subplot(2,1,2)
-pp = plot(lim_inf:lim_sup,out_val(lim_inf:lim_sup),lim_inf:lim_sup,y_cha(k,lim_inf:lim_sup));
+pp = plot(lim_inf:lim_sup,out_val(lim_inf:lim_sup),lim_inf:lim_sup,y_cha(rho,lim_inf:lim_sup));
 pp(1).Color = 'b';
 pp(2).Color = 'r';
 title('Saída Real x Saída predita')
